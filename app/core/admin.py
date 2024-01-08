f48cce63f64b574a78551a60bc7588dc4011ff4d
K Shyam Kumar Reddy <kshyamrdy@gmail.com>
2024-01-08 15:11:27 +0530

    linting fixed

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,10 +4,9 @@ Django admin customization.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-
 from core import models
- 
- 
+
+
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
@@ -43,5 +42,4 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
  
-
 admin.site.register(models.User, UserAdmin)
